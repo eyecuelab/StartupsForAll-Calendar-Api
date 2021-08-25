3ca33ca8983486aa872465c0418a50620bb7223b
Usarneme <jamestcoop@gmail.com>
2021-08-25 12:02:52 -0700

    :test_tube: :white_check_mark: adds passing tests to events.controller.spec for create, remove, update, findAll, and findOne controller route handlers

--- src/events/events.controller.spec.ts
+++ src/events/events.controller.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getConnectionToken, TypeOrmModule } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
+import { Connection, DeleteResult, UpdateResult } from 'typeorm';
 import ormconfig from '../database/ormconfig';
 import { Event } from './entities/event.entity';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { CreateEventDto } from './dto/create-event.dto';
 
 describe('EventsController', () => {
   let eventsController: EventsController;
@@ -37,5 +39,31 @@ describe('EventsController', () => {
     let result: Promise<Event[]>;
     jest.spyOn(eventsService, 'findAll').mockImplementation(() => result);
     expect(await eventsController.findAll()).toBe(result);
+  });
+
+  it('eventsController.findOne() should return a single event', async () => {
+    let result: Promise<Event>;
+    jest.spyOn(eventsService, 'findOne').mockImplementation(() => result);
+    expect(await eventsController.findOne('fakeuuid')).toBe(result);
+  });
+
+  it('eventsController.update() should return an UpdateResult of an event', async () => {
+    let result: Promise<UpdateResult>;
+    let fakeUpdateDto: UpdateEventDto;
+    jest.spyOn(eventsService, 'updateOne').mockImplementation(() => result);
+    expect(await eventsController.update('fakeuuid', fakeUpdateDto)).toBe(result);
+  });
+
+  it('eventsController.remove() should delete a single event', async () => {
+    let result: Promise<DeleteResult>;
+    jest.spyOn(eventsService, 'remove').mockImplementation(() => result);
+    expect(await eventsController.remove('fakeuuid')).toBe(result);
+  });
+
+  it('eventsController.create() should add a single event', async () => {
+    let result: Promise<Event>;
+    let fakeEvent: CreateEventDto;
+    jest.spyOn(eventsService, 'create').mockImplementation(() => result);
+    expect(await eventsController.create(fakeEvent)).toBe(result);
   });
 });
