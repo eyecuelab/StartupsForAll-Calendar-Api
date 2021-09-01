7d9c1f2a2d475c47ea9eab64914ee3272ff51d36
Usarneme <jamestcoop@gmail.com>
2021-09-01 08:26:32 -0700

    :construction: updates local.strategy to disallow logging in event 'eventKey' user

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -10,6 +10,8 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string): Promise<any> {
+    // TODO: this feels hacky. maybe: if(username !== 'admin') ?
+    if (username === 'eventKey') return false;
     const user = await this.authService.validateUser(username, password);
     if (!user) {
       throw new UnauthorizedException();
