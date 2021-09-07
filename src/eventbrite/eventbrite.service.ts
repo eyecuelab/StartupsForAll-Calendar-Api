70ee1d77b331bf70c0ae13c659a793bc09eb949a
Usarneme <jamestcoop@gmail.com>
2021-09-07 11:43:46 -0700

    :recycle: refactors cost to only include base, not possible taxes or fees

--- src/eventbrite/eventbrite.service.ts
+++ src/eventbrite/eventbrite.service.ts
@@ -18,10 +18,7 @@ export class EventbriteService {
           name: res.data.name.text,
           created: res.data.created,
           changed: res.data.changed,
-          cost: (
-            Number(res.data?.ticket_classes[0]?.cost.display.substring(1)) +
-            Number(res.data?.ticket_classes[0]?.fee.display.substring(1))
-          ).toFixed(2),
+          cost: res.data?.ticket_classes[0]?.cost.display,
           currency: res.data.currency,
           description: res.data.description.text,
           summary: res.data.summary,
