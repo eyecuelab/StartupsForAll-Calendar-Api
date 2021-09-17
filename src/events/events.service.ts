549e1f44f771fbd77c153911cec6b2e5b5171d35
Usarneme <jamestcoop@gmail.com>
2021-09-17 16:41:10 -0700

    :tada: updates events query dto and events service to add filtering by optional query string passed to /events GET route

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -13,68 +13,97 @@ export class EventsService {
     private eventsRespository: Repository<Event>
   ) {}
 
-  // async findAll(): Promise<Event[]> {
-  //   return this.eventsRespository.find();
-  // }
-
   async findAll(query: EventsQueryDto): Promise<Event[]> {
-    // const qb = getRepository(Winecellar)
-    //         .createQueryBuilder("wine")
-    //         .orderBy("wine.Vintage", "ASC");
-
-    // if (colour !== 'any') {
-    //   qb.andWhere("wine.Colour = :Colour", { Colour: colour });
-    // }
-    // if (grape !== 'any') {
-    //   qb.andWhere("wine.Grape = :Grape", { Grape: grape });
-    // }
-
-    // const wineSelection = await qb.getRawMany();
     console.log('hit find all service w/query:', query);
-    const qb = this.eventsRespository
-      .createQueryBuilder()
-      .select('events')
-      .from(Event, 'events')
-      .orderBy('events.start_date', 'ASC');
-    return await qb.getMany();
-  }
-
-  async findByQuery(): Promise<Event[]> {
-    const result = await this.eventsRespository
-      .createQueryBuilder()
-      // select from the events table
-      .select('event')
-      // create an alias of type Event called event, used later in the query
-      .from(Event, 'event')
-      // use :title as a parameter instead of interpolating string as that allows sql injection
-      .where('event.title = :title', { title: 'Test' })
-      // optional additional where filters
-      // .andWhere('event.blah = :alsoMe', { blah: 'Something Else' })
-      // ascending, descending results
-      .orderBy('event.start_date', 'ASC')
-      // optional secondary/tertiary ordering
-      // .addOrderBy('event.title', 'ASC')
-      // option to print the raw sql to console, used for testing
-      .printSql()
-      // get more than one, maybe use getManyOrFail() instead?
-      .getMany();
-    console.log('findByQuery got result:', result);
-    return result;
-  }
-
-  async findByCategory(category: string): Promise<Event[]> {
-    console.log('hit the find by category service');
-    return await this.eventsRespository
-      .createQueryBuilder()
-      .select('event')
-      .from(Event, 'events')
-      .where('events.category = :category', { category: category })
-      // .orderBy('events.start_date', 'ASC')
-      .getMany();
-
-    //   where({
-    // ...(name && {name: 'butternut squash'}),
-    // })
+    const {
+      agenda,
+      audience,
+      category,
+      category_text,
+      cost,
+      creator_email,
+      creator_name,
+      custom_blurb,
+      description,
+      location,
+      logo,
+      start_date,
+      end_date,
+      start_time,
+      end_time,
+      panelists,
+      promoted,
+      summary,
+      title,
+      topics,
+      url,
+    } = query;
+    // eslint-disable-next-line prefer-const
+    let qb = this.eventsRespository.createQueryBuilder().select('events').from(Event, 'events').where('1=1');
+    if (agenda) {
+      qb.andWhere('events.agenda = :agenda', { agenda: agenda });
+    }
+    if (audience) {
+      qb.andWhere('events.audience = :audience', { audience: audience });
+    }
+    if (category) {
+      qb.andWhere('events.category = :category', { category: category });
+    }
+    if (category_text) {
+      qb.andWhere('events.category_text = :category_text', { category_text: category_text });
+    }
+    if (cost) {
+      qb.andWhere('events.cost = :cost', { cost: cost });
+    }
+    if (creator_email) {
+      qb.andWhere('events.creator_email = :creator_email', { creator_email: creator_email });
+    }
+    if (creator_name) {
+      qb.andWhere('events.creator_name = :creator_name', { creator_name: creator_name });
+    }
+    if (custom_blurb) {
+      qb.andWhere('events.custom_blurb = :custom_blurb', { custom_blurb: custom_blurb });
+    }
+    if (description) {
+      qb.andWhere('events.description = :description', { description: description });
+    }
+    if (location) {
+      qb.andWhere('events.location = :location', { location: location });
+    }
+    if (logo) {
+      qb.andWhere('events.logo = :logo', { logo: logo });
+    }
+    if (start_date) {
+      qb.andWhere('events.start_date = :start_date', { start_date: start_date });
+    }
+    if (end_date) {
+      qb.andWhere('events.end_date = :end_date', { end_date: end_date });
+    }
+    if (start_time) {
+      qb.andWhere('events.start_time = :start_time', { start_time: start_time });
+    }
+    if (end_time) {
+      qb.andWhere('events.end_time = :end_time', { end_time: end_time });
+    }
+    if (panelists) {
+      qb.andWhere('events.panelists = :panelists', { panelists: panelists });
+    }
+    if (promoted) {
+      qb.andWhere('events.promoted = :promoted', { promoted: promoted });
+    }
+    if (summary) {
+      qb.andWhere('events.summary = :summary', { summary: summary });
+    }
+    if (title) {
+      qb.andWhere('events.title = :title', { title: title });
+    }
+    if (topics) {
+      qb.andWhere('events.topics = :topics', { topics: topics });
+    }
+    if (url) {
+      qb.andWhere('events.url = :url', { url: url });
+    }
+    return await qb.orderBy('events.start_date', 'ASC').getMany();
   }
 
   async create(eventData: CreateEventDto): Promise<Event> {
