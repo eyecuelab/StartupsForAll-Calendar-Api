71bfa141b160f18a2871ea9ead2c9dae48819635
Usarneme <jamestcoop@gmail.com>
2021-09-20 15:52:27 -0700

    updates data migration to only drop event if that table exists

--- src/database/migrations/1632162085547-AddedToGCalDefaultValNull.ts
+++ src/database/migrations/1632162085547-AddedToGCalDefaultValNull.ts
@@ -4,6 +4,7 @@ export class AddedToGCalDefaultValNull1632162085547 implements MigrationInterfac
   name = 'AddedToGCalDefaultValNull1632162085547';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE IF EXISTS "event"`);
     await queryRunner.query(
       `CREATE TABLE "event" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "in_google_cal" date, "category" character varying NOT NULL, "category_text" character varying NOT NULL, "cost" integer NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "creator_email" text, "creator_name" text, "custom_blurb" text NOT NULL, "location" text NOT NULL, "logo" text, "start_date" date, "end_date" date, "promoted" boolean, "summary" text NOT NULL, "title" text NOT NULL, "topics" text NOT NULL, "url" text, CONSTRAINT "PK_30c2f3bbaf6d34a55f8ae6e4614" PRIMARY KEY ("id"))`
     );
